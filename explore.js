0e00eb11398e5c081a7ce056593fd00dec1642bb
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-01 17:03:52 -0400

    Add error Catch to promise

--- explore.js
+++ explore.js
@@ -5,6 +5,6 @@ const client = new Twilio('Account_SID', 'auth_token')
 client.messages.list()
     .then(messages => 
         console.log(`The most recent message is ${messages[0].body}`)
-    );
+    ).catch(err => console.error(err));
 
 console.log('Gathering your message log');
