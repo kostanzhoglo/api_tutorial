a360f3d57faef70feede95005e3e63d761e9c26a
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-01 16:57:11 -0400

    Add promise to print most recent message

--- explore.js
+++ explore.js
@@ -1,3 +1,10 @@
 const Twilio = require("twilio");
 
 const client = new Twilio('Account_SID', 'auth_token')
+
+client.messages.list()
+    .then(messages => 
+        console.log(`The most recent message is ${messages[0].body}`)
+    );
+
+console.log('Gathering your message log');
